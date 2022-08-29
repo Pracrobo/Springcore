6d6c82743cefaab722ad4bc1e31070a2eec97eba
Pracrobo <cpu180621@gmail.com>
2022-08-29 16:48:56 +0900

    05. test 까지 완료, 실제 적용 다음시간

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -5,7 +5,8 @@ import java.util.Map;
 
 public class MemoryMemberRepository implements MemberRepository  {
 // 테스트용
-    private static Map<Long, Member> store = new HashMap<>(); //ConcurrentHashMap() : DI
+    private static Map<Long, Member> store = new HashMap<>();
+    //ConcurrentHashMap() : DI 동시성 이슈가 있을 수 있기 때문에
 
     @Override
     public void save(Member member) {
