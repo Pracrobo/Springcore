c11a865fa2e85db1b81ee66eb4a8f5c6e68595f8
Pracrobo <cpu180621@gmail.com>
2024-03-28 16:35:48 +0900

    240328 lombok 문제 해결

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,19 +3,17 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor //생성자 대신 만들어줌
 public class OrderServiceImpl implements OrderService {
 
+    // 테스트 용도
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -23,9 +21,5 @@ public class OrderServiceImpl implements OrderService {
         int discountPrice = discountPolicy.discount(member, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
-    }
-    // 테스트 용도
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
     }
 }
